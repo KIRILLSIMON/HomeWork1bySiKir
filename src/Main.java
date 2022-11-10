9f481034d398cb4f4e65a9f347dbafddcc89dca7
sikir <sikir2022@gmail.com>
2022-11-10 12:45:46 +0300

    Настоящая первая моя попытка переслать ДЗ1...

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        /*int dog=8;
+            double cat=3.6;
+            double papper=763789;
+            dog+=4;
+            cat+=4;
+            papper+=4;
+            var friend=19;
+            friend=friend+2;
+            friend=friend/3;
+            var frog=3.5;
+            frog=(frog*10)/3.5;
+            System.out.println(frog);
+            System.out.println(friend);
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(papper);
+            float dog2 =dog-3.5;
+            float cat2=cat-1.6;
+            int papper2=papper-7639;
+            System.out.println(dog2);
+            System.out.println(cat2);
+            System.out.println(papper2);
+            */
+        /*var weightP1=78.2;
+        var weightP2=82.7;
+        var overloat=(weightP2%weightP1);
+        var totalweight= weightP1 + weightP2;
+       /* var deltaWeight =weightP2-weightP1;
+        System.out.println("разница веса спортсменов в кг составила:"+ " "+ deltaWeight);*/
+        /*var deltaweight=weightP2%weightP1;
+        System.out.println("total weight in kg is(=) "+" " +totalweight);
+        System.out.println("перевес составил(в кг): "+ " "+ overloat);*/
+        System.out.println("Привет моим уважаемым наставникам!Пришлите ответ-на проверку обратной связи-на мой приём.");
+        var alltime=640;
+        var worktime=8;
+        int stateAre= (alltime/worktime);
+        System.out.println(stateAre);
+        int newstate= stateAre+94;
+        int newtime=newstate*8;
+        System.out.println(newstate);
+        System.out.println(newtime);
+
+        var stateAreNew= stateAre+94;
+        System.out.println(stateAreNew);
+    }
+}
